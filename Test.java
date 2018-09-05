7c0a4ede0fbe3c7f569c4f15b537503ba1fcf1f4
juyong kim <Juyong.Kim@uon.edu.au>
2018-09-05 14:33:14 +1000

    woot

--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@ Starting anew
 import java.util.List;
 import java.util.LinkedList;
 import java.nio.file.*;
+import java.io.IOException;
 
 
 public class Test
@@ -14,73 +15,78 @@ public class Test
     private static NRR nrr;
     private static FB fb;
 
-    public static void main(String[] args)
+        // method to read file and store its data as a String
+        public static String readFileAsString(String inputFile)
+        {
+            String data = null;
+
+                try 
+                {
+                        data = new String(Files.readAllBytes(Paths.get(inputFile)));
+                } 
+                catch (IOException e)           
+                {
+                        //TODO: handle exception
+                        System.err.println(e);
+                }
+            return data;
+        }
+
+    public static void main(String[] args) 
     {
 
         fcfs = new FCFS();
         rr = new RR();
         nrr = new NRR();
         fb = new FB();
+        List id = new LinkedList<String>();
+        List arrival = new LinkedList();
+        List execute = new LinkedList();
+        int dispatcher, counter = 0;
+        String data = readFileAsString("datafile1.txt");            // pulls data from input file and stores in String 'data'
+        String[] parts = data.split("\\s+");              // splits data by word into an array
 
-        fb.addDis(1);
-        fb.addProcess("p1",0,10);
-        fb.addProcess("p2",0,1);
-        fb.addProcess("p3",0,2);
-        fb.addProcess("p4",0,1);
-        fb.addProcess("p5",0,5);
-        fb.doStuff();
+        for(int t = 0; t <= parts.length;t++)
+        {
+                if(parts[t] == "DISP:")
+                {
+                    dispatcher = Integer.parseInt(parts[t+1]);    
+                }
+                if(parts[t] == "ID:")
+                {
+                        id.add(parts[t+1]);
+                        counter++;
+                }
+                if(parts[t] == "Arrive:")
+                {
+                        arrival.add(Integer.parseInt(parts[t+1]));
+                }
+                if(parts[t] == "ExecSize:")
+                {
+                        execute.add(Integer.parseInt(parts[t+1]));
+                }
+        }
 
-        fb.addDis(1);
-        fb.addProcess("p1",0,10);
-        fb.addProcess("p2",2,1);
-        fb.addProcess("p3",6,2);
-        fb.addProcess("p4",10,1);
-        fb.addProcess("p5",14,5);
-        fb.doStuff();       
 
-/*
-        nrr.addDis(1);
-        nrr.addProcess("p1",0,10);
-        nrr.addProcess("p2",0,1);
-        nrr.addProcess("p3",0,2);
-        nrr.addProcess("p4",0,1);
-        nrr.addProcess("p5",0,5);
-        nrr.doStuff();
 
-        nrr.addDis(1);
-        nrr.addProcess("p1",0,10);
-        nrr.addProcess("p2",2,1);
-        nrr.addProcess("p3",6,2);
-        nrr.addProcess("p4",10,1);
-        nrr.addProcess("p5",14,5);
-        nrr.doStuff();
-*/
-/*
-        rr.addDis(1);
-        rr.addProcess("p1",0,10);
-        rr.addProcess("p2",0,1);
-        rr.addProcess("p3",0,2);
-        rr.addProcess("p4",0,1);
-        rr.addProcess("p5",0,5);
-        rr.doStuff();
-
-        rr.addDis(1);
-        rr.addProcess("p1",0,10);
-        rr.addProcess("p2",2,1);
-        rr.addProcess("p3",6,2);
-        rr.addProcess("p4",10,1);
-        rr.addProcess("p5",14,5);
-        rr.doStuff();
-*/
-/*
-        fcfs.addDis(1);
-        fcfs.addProcess("p1",0,10);
-        fcfs.addProcess("p2",0,1);
-        fcfs.addProcess("p3",0,2);
-        fcfs.addProcess("p4",0,1);
-        fcfs.addProcess("p5",0,5);
+        fcfs.addDis(dispatcher);
+        rr.addDis(dispatcher);
+        fb.addDis(dispatcher);
+        nrr.addDis(dispatcher);
+        
+        for(int i = 0; i <= counter;i++)
+        {
+                fcfs.addProcess(id.get(i),arrival.get(i),execute.get(i));
+                 rr.addProcess(id.get(i),arrival.get(i),execute.get(i));
+                 fb.addProcess(id.get(i),arrival.get(i),execute.get(i));
+                 nrr.addProcess(id.get(i),arrival.get(i),execute.get(i));
+        }
+    
         fcfs.doStuff();
-*/        
+        rr.doStuff();
+        fb.doStuff();
+        nrr.doStuff();
+      
 
     }//main
 
